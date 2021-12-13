f8ffbb4d2796907d9d8919a8fc8b166ff05ee953
escowin <edwin@escowinart.com>
2021-12-12 18:47:48 -0600

    js psuedocode and code functions 3-6

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-// #1 current day is displayed at the top of the calendar
+// #1 current day is displayed at the top of the calendar using moment.js formatting
 $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 // #2 presented with time blocks for standard business hours
 var dailyEvents = {
-    date: (moment().format('MMMM Do YYYY, h:mm:ss a');
+    date: moment().format('MMMM Do YYYY'),
     09: "",
     10: "",
     11: "",
@@ -11,16 +11,48 @@ var dailyEvents = {
     13: "",
     14: "",
     15: "",
-    16: "",
+    16: ""
 };
 
 // #3 each time block is color-coded to indicate whether it is in the past, present, or future
 var timeOfDay = function() {
     var currentHour = moment().hour();
     var textAreaEl = document.querySelectorAll("textarea");
-}
-// #4 click into time block, enter an event
+    for (var i = 0; i < textAreaEl.length; i++) {
+        if (parseInt(textAreaEl[i].getAttribute("data-hour")) > currentHour) {
+            textAreaEl[i].classList.add("future");
+        }
+        else if (parseInt(textAreaEl[i].getAttribute("data-hour")) > currentHour) {
+            textAreaEl[i].classList.add("past");
+        }
+        else (parseInt(textAreaEl[i].getAttribute("data-hour")) > currentHour) {
+            textAreaEl[i].classList.add("present");
+        }       
+    }
+};
 
 // #5 click save, text for that event is saved in local storage
+var saveEvents = function(hour, text) {
+    var keepCurrentStorage = checkLocalStorage();
+    if (!keepCurrentStorage) {
+        dailyEvents[hour] = text;
+        localStorage.setItem("Scheduler", JSON.stringify(dailyEvents));
+    }
+    // else () {}???
+};
 
 // #6 refresh page, saved events persist
+var checkLocalStorage = function() {
+
+};
+var loadEvents = function() {
+
+};
+
+// #4 event listener to make above happen
+$('.saveBtn').click(function(){
+    var hour = $(this).parent().attr('id');
+    var text = $(this).parent().children('textarea').val();
+});
+
+timeOfDay();
