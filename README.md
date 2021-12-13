44b02a3164000b6d766f42ce1566c4b24a30e8bf
escowin <edwin@escowinart.com>
2021-12-12 22:24:22 -0600

    added hour 17 (5pm) in js daily events, html tweaks

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+
+
 # Work Day Scheduler Starter Code
 <!-- GIVEN I am using a daily planner to create a schedule
 WHEN I open the planner
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
 // #1 current day is displayed at the top of the calendar using moment.js formatting
-$('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-console.log($)
+$("#currentDay").text(moment().format('dddd, MMMM Do YYYY | h:mm:ss a'));
 
 // #2 presented with time blocks for standard 9.00a-5.00p business hours
 var dailyEvents = {
-    date: moment().format('MMMM Do YYYY'),
+    date: moment().format('dddd, MMMM Do YYY'),
     09: "",
     10: "",
     11: "",
@@ -12,7 +11,8 @@ var dailyEvents = {
     13: "",
     14: "",
     15: "",
-    16: ""
+    16: "",
+    17: ""
 };
 
 // #3 each time block is color-coded to indicate whether it is in the past, present, or future
@@ -23,12 +23,12 @@ var timeOfDay = function() {
         if (parseInt(textAreaEl[i].getAttribute("data-hour")) > currentHour) {
             textAreaEl[i].classList.add("future");
         }
-        else if (parseInt(textAreaEl[i].getAttribute("data-hour")) > currentHour) {
+        else if (parseInt(textAreaEl[i].getAttribute("data-hour")) < currentHour) {
             textAreaEl[i].classList.add("past");
         }
-        else (parseInt(textAreaEl[i].getAttribute("data-hour")) > currentHour) {
+        else {
             textAreaEl[i].classList.add("present");
-        }       
+        }
     }
 };
 
@@ -37,14 +37,17 @@ var saveEvents = function(hour, text) {
     var keepCurrentStorage = checkLocalStorage();
     if (!keepCurrentStorage) {
         dailyEvents[hour] = text;
-        localStorage.setItem('scheduler', JSON.stringify(dailyEvents));
+        localStorage.setItem("Scheduler", JSON.stringify(dailyEvents));
+    } else {
+        var schedule = JSON.parse(localStorage.getItem("Scheduler"));
+        schedule[hour] = text;
+        localStorage.setItem("Scheduler", JSON.stringify(schedule));
     }
-    // else () {}???
 };
 
 // #6 refresh page, saved events persist
 var checkLocalStorage = function() {
-    var schedule = JSON.parse(localStorage.getItem('scheduler'));
+    var schedule = JSON.parse(localStorage.getItem("Scheduler"));
     if (!schedule) {
         return false;
     } else if (schedule.date !== dailyEvents.date) {
@@ -56,23 +59,31 @@ var checkLocalStorage = function() {
 
 // loading localStorage saved events into scheduler
 var loadEvents = function() {
-    var schedule = JSON.parse(localStorage.getItem('scheduler'));
+    var schedule = JSON.parse(localStorage.getItem("Scheduler"));
     if (schedule) {
-        if (schedule.date === moment().format('MMMM Do YYYY')) {
+        if (schedule.date === moment().format('dddd, MMMM Do YYYY')) {
             for (var i = 9; i < 18; i++) {
-                $('#' + i).children('textarea').val(schedule[i]);
+                $("#" + i).children("textarea").val(schedule[i]);
             }
         }
     }
 };
 
 // #4 event listener to make #5 & #6 happen
-$('.saveBtn').click(function(){
-    var hour = $(this).parent().attr('id');
-    var text = $(this).parent().children('textarea').val();
+$(".saveBtn").click(function() {
+    var hour = $(this).parent().attr("id");
+    var text = $(this).parent().children("textarea").val();
     if (!text) {
-        alert('nothing to save');
-    }
+        // 4a save will not occur without text input
+        alert('enter an event before saving');
+    } saveEvents(hour, text);
+        //4b event saved indicated by appearing & disappearing bootstrap icon
+    $("#saved").html('<i class="bi bi-calendar-check-fill"> ᴇᴠᴇɴᴛ s a v e d</i>');
+    setInterval(function() {
+        $("#saved").html("");
+    }, 2000);
 });
 
 timeOfDay();
+setInterval(timeOfDay, (1000 * 60));
+loadEvents();
